b331eb3e3fa69568d6096570c301119bc698c7c6
Sam Germain <sam.germain@usask.ca>
2022-05-06 16:59:11 -0600

    split_transients transient locations is a list of tuples, more type checks

--- src/split_transients.py
+++ src/split_transients.py
@@ -1,7 +1,7 @@
 # import matplotlib.pyplot as plt
 import os
 import shutil
-from typing import List, Optional
+from typing import List, Optional, Tuple
 
 import librosa
 import numpy as np
@@ -11,22 +11,23 @@ import soundfile as sf
 # from config import assets
 
 
-def get_onsets_from_midi(midi_file: str, sr: int):
+def get_onsets_from_midi(midi_file: str, sr: int) -> List[int]:
     midi_data = pretty_midi.PrettyMIDI(midi_file)
     onsets = midi_data.get_onsets()
     arr = np.array(onsets)
     return librosa.core.time_to_samples(times=arr, sr=sr)
 
 
-def get_onsets(y: np.ndarray, sr: int):
+def get_onsets(y: np.ndarray, sr: int) -> List[int]:
     C = np.abs(librosa.cqt(y=y, sr=sr))
     o_env = librosa.onset.onset_strength(sr=sr, S=librosa.amplitude_to_db(C, ref=np.max))
     onsets = librosa.onset.onset_detect(onset_envelope=o_env, sr=sr)
     return librosa.frames_to_samples(onsets)
 
 
-def onsets_to_transient_locations(onset_samples: List[int]) -> List[List[int]]:
-    """Takes a list of onset times for an audio file and returns the list of start and stop times for that audio file
+def onsets_to_transient_locations(onset_samples: List[int]) -> List[Tuple[int, int]]:
+    """
+    Takes a list of onset times for an audio file and returns the list of start and stop times for that audio file
     :param [int] onset_samples: I don't really know what these are actually.
         I thought they were start times for each sound change but I don't know
     :return: A list of start and stop times for each sound change
@@ -36,23 +37,28 @@ def onsets_to_transient_locations(onset_samples: List[int]) -> List[List[int]]:
     stops = onset_samples[1:]
     transient_times = []
     for s in range(len(starts)):
-        transient_times.append([starts[s], stops[s]])
+        transient_times.append((starts[s], stops[s]))
     return transient_times
 
 
-def locations_to_samples(y: np.ndarray, transient_locations):
+def locations_to_samples(
+    y: np.ndarray,
+    transient_locations: List[Tuple[int, int]]
+) -> List[np.ndarray]:
+
     transient_samples = []
 
-    for time in transient_locations:
-        transient_samples.append(y[time[0]:time[1]])
+    for (start, end) in transient_locations:
+        transient_samples.append(y[start:end])
+
     return transient_samples
 
 
 def get_transient_locations(
-    y: np.ndarray,
+    y: Optional[np.ndarray] = None,
     midi_file: Optional[str] = None,
     sr: int = 44100
-) -> List[List[int]]:
+) -> List[Tuple[int, int]]:
     """
     Takes the path to an audio file and returns the list of start and stop times for that
     audio file as a frame rate. The midi file would have the correct start and stop times.
@@ -66,9 +72,11 @@ def get_transient_locations(
     if midi_file:
         onsets = get_onsets_from_midi(midi_file, sr)
     else:
+        if y is None:
+            raise Exception('get_transient_locations must have one of y or midi_file')
         onsets = get_onsets(y, sr)
-    transient_locations = onsets_to_transient_locations(onsets)
-    return transient_locations
+
+    return onsets_to_transient_locations(onsets)
 
 
 def save_transients(
@@ -89,7 +97,11 @@ def save_transients(
         sf.write(filename, transient_samples[sample], sr)
 
 
-def locations_to_spectrograms(y: np.ndarray, locations):
+def locations_to_spectrograms(
+    y: np.ndarray,
+    locations: List[Tuple[int, int]]
+) -> List[np.ndarray]:
+
     samples = locations_to_samples(y, locations)
     return [np.abs(librosa.stft(y)) for y in samples]
 

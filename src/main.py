2bf82412b8903e45b3f2126db490540a4aa61c41
Sam Germain <sam.germain@usask.ca>
2022-05-06 18:27:11 -0600

    updated command line parser

--- src/main.py
+++ src/main.py
@@ -17,15 +17,10 @@ def ez_load(sound_file: str, sr: Optional[int] = None):
 
 
 parser = argparse.ArgumentParser(
-    description=(
-        'Create a plot of each sound in the sound, sorted by sound similarity. '
-        '-i infile (required)'
-        '-o outfile'
-        '-m output from Ableton right click -> slice to new midi track'
-        '-s sample rate'
-    )
+    description='Create a plot of each sound in the sound, sorted by sound similarity'
 )
-parser.add_argument(
+required = parser.add_argument_group('required arguments')
+required.add_argument(
     '-i',
     '--infile',
     type=str,
@@ -38,8 +33,14 @@ parser.add_argument(
     default='outfile.mid',  # The midi file to write to
 )
 # If there is a midi file to base start and stop times off of
-parser.add_argument('-m', '--midi', type=str, default=None)
-parser.add_argument('-s', '--sr', type=int, default=44100)
+parser.add_argument(
+    '-m',
+    '--midi',
+    type=str,
+    default=None,
+    help='Output from Ableton right click -> slice to new midi track',
+)
+parser.add_argument('-s', '--sr', type=int, default=44100, help='sample rate')
 args = parser.parse_args()
 
 y, sr = ez_load(args.infile, args.sr)
